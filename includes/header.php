21bec2f18ae30de80a8897f69fc091aadc46e1b6
sebert23 <sandra.ebert@gmx.de>
2023-03-10 21:06:57 +0100

    Refactoring of style files

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="ABI Kinder- und Jugendfußball
+  Homepage des Fördervereins für die Fußballjugend des SC Abstatt,
+  TGV Eintracht Beilstein, SC Ilsfeld und des SPVGG Heinriet" />
+  <link rel="icon" href="images/ABI Logo-3-2.1.png" type="image/png">
+  <link rel="apple-touch-icon" href="images/ABI Logo-3-2.1.png" type="image/png" size="180x180">
+  <title>SGM ABI Kinder- und Jugendfußball in Abstatt, Beilstein, Ilsfeld und Heinriet</title>
+  <link rel="stylesheet" href="Index_style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
