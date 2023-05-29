5b9a4941bdcedfb5fe129c89d3a60871d3f64614
Sandra Ebert <sandra.ebert@gmx.de>
2023-05-29 12:54:32 +0200

    Skript als ausführbare Datei im Terminal

--- Skripte/Extract_table_from_fussball.py
+++ Skripte/Extract_table_from_fussball.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[8]:
-
 import os
 import requests
 import csv
@@ -12,131 +10,180 @@ import pandas as pd
 import datetime
 from datetime import date
 
+team_urls = pd.read_csv("Spiele_Links.csv")
+outfile = "All_games_from_fussball_de.csv"
+numOfGames = 0
 
-# In[58]:
-
-team_urls = pd.read_csv('Spiele_Links.csv')
-outfile = "out_new.csv";
-os.rename(outfile, "out_old.csv")
-
-for idx in range(0,len(team_urls)):
-
-    url = team_urls['url'][idx]
+# rufe alle links aus Spiele_links.csv auf um für jede Mannschaft die noch stattfindenden Spiele zu extrahieren
+for idx in range(0, len(team_urls)):
+    # load url
+    url = team_urls["url"][idx]
     response = requests.get(url)
+    df = pd.DataFrame()
 
-    #BeautifulSoup HTML-Dokument aus dem Quelltext parsen
-    soup= BeautifulSoup(response.text, 'html.parser')
+    # BeautifulSoup HTML-Dokument aus dem Quelltext parsen
+    soup = BeautifulSoup(response.text, "html.parser")
 
-    #Tabelle aus dem HTML-Dokument extrahieren
-    table = soup.find('div', {"id":'id-team-matchplan-table'})
-    #print(table)
+    # Tabelle mit den Spielen aus dem HTML-Dokument extrahieren
+    table = soup.find("div", {"id": "id-team-matchplan-table"})
 
-    team=team_urls['team'][idx]
+    # extract id of team i.e. B1, C2,...
+    team = team_urls["team"][idx]
     print(team)
-    
-    #Datum und Liga auslesen (befindet sich in jeder 3. Zeile ab 2. Zeile)
-    for data in table.find_all('tbody'):
-        rows = data.find_all('tr')
+
+    # Datum und Liga auslesen (befindet sich in jeder 3. Zeile ab 2. Zeile)
+    for data in table.find_all("tbody"):
+        rows = data.find_all("tr")
         dates = list()
         times = list()
         teams = list()
         kw = list()
-        for i in range(1,len(rows),3):
-            #entfernt Sonderzeichen wie Zeilenumbruch (n) und Tab (t)
-            date = rows[i].find('td', class_='column-date') #rows[i].text
-            cleanString = re.sub(r'\ \|',',', date.text)
-            cleanString = re.sub(r' ', '', cleanString)
-            #removedSpace = re.sub(r'\t', '', removedSpace)
-            splitted= cleanString.split(',')
-            date_split = splitted[1].split('.')
-            datum = datetime.date(int(date_split[2])+2000, int(date_split[1]), int(date_split[0]))
+        for i in range(1, len(rows), 3):
+            # entfernt Sonderzeichen wie Zeilenumbruch (n) und Tab (t)
+            date = rows[i].find("td", class_="column-date")  # rows[i].text
+            cleanString = re.sub(r"\ \|", ",", date.text)
+            cleanString = re.sub(r" ", "", cleanString)
+            splitted = cleanString.split(",")
+
+            # Datum wird extrahiert um die KW auszurechnen und Datum und Uhrzeit separat abzuspeichern
+            date_split = splitted[1].split(".")
+            datum = datetime.date(
+                int(date_split[2]) + 2000, int(date_split[1]), int(date_split[0])
+            )
             kw.append(datum.isocalendar()[1])
             dates.append(splitted[1])
             times.append(splitted[2])
+            # add team id
             teams.append(team)
-            
-    df = pd.DataFrame(list(zip(dates,times,teams)),columns=['Datum','Zeit','Team'])
-    df.insert(len(df.columns), 'KW', kw)
-    df
 
+    # Listen mit Datum, Uhrzeit und Team-ID werden in einer Tabelle gespeichert
+    df = pd.DataFrame(list(zip(dates, times, teams)), columns=["Datum", "Zeit", "Team"])
+    df.insert(len(df.columns), "KW", kw)
 
-    # In[62]:
-
-
-    #Club und Logo auslesen (befindet sich in jeder 3. Zeile ab 3. Zeile)
-    for data in table.find_all('tbody'):
-        rows = data.find_all('tr')
+    # Gegner (Fußballvereine) auslesen (befindet sich in jeder 3. Zeile ab 3. Zeile)
+    for data in table.find_all("tbody"):
+        rows = data.find_all("tr")
+        # 1. Mannschaft (Heim)
         club1 = list()
+        # 2. Mannschaft (auswärts)
         club2 = list()
-        for i in range(2,len(rows),3):
-            #entfernt Sonderzeichen wie Zeilenumbruch (n) und Tab (t)
-            #print(rows[i].text)
-            removedSpace = re.sub(r'\n', '', rows[i].text)
-            removedSpace = re.sub(r'\t', '', removedSpace)
-            removedSpace = re.sub(r':', ';', removedSpace)
-            splitted = removedSpace.split(';')
+        for i in range(2, len(rows), 3):
+            # entfernt Sonderzeichen wie Zeilenumbruch (n) und Tab (t)
+            # print(rows[i].text)
+            removedSpace = re.sub(r"\n", "", rows[i].text)
+            removedSpace = re.sub(r"\t", "", removedSpace)
+            removedSpace = re.sub(r":", ";", removedSpace)
+            splitted = removedSpace.split(";")
             club1.append(splitted[0])
             club2.append(splitted[1][0:-1])
 
+    # Abspeichern der Heim und Gast-Mannschaft in der Tabelle mit den Spieldaten
     df.insert(len(df.columns), "Heim", club1)
     df.insert(len(df.columns), "Gast", club2)
 
-
-    # In[63]:
-
-
-    #Datum und Liga auslesen (befindet sich in jeder 3. Zeile ab 2. Zeile)
-    for data in table.find_all('tbody'):
-        rows = data.find_all('tr')
+    # Logo von den Mannschaften auslesen (befindet sich in jeder 3. Zeile ab 2. Zeile)
+    for data in table.find_all("tbody"):
+        rows = data.find_all("tr")
         logos_home = list()
-        logos_guest= list()
-        for i in range(2,len(rows),3):
-            #entfernt Sonderzeichen wie Zeilenumbruch (n) und Tab (t)
-            logo = rows[i].find_all('div', class_="club-logo") #rows[i].text
-            
-            logo_link = re.findall(r'//(\S+)\">', str(logo))
+        logos_guest = list()
+        for i in range(2, len(rows), 3):
+            # entfernt Sonderzeichen wie Zeilenumbruch (n) und Tab (t)
+            logo = rows[i].find_all("div", class_="club-logo")  # rows[i].text
+
+            logo_link = re.findall(r"//(\S+)\">", str(logo))
             logos_home.append(logo_link[0])
             logos_guest.append(logo_link[1])
-            
-    #found = re.search('AAA(.+?)ZZZ', string).group(1)
 
-    df.insert(len(df.columns), 'Logo Heim', logos_home)
-    df.insert(len(df.columns), 'Logo Gast', logos_guest)
+    # Logo-URLs in der Tabelle abspeichern
+    df.insert(len(df.columns), "Logo Heim", logos_home)
+    df.insert(len(df.columns), "Logo Gast", logos_guest)
 
+    # Alle Links zum Spiel und zu den Mannschaften auf fussball.de auslesen
+    links = table.find_all("a")
+    homeLinks = list()
+    guestLinks = list()
+    spielLinks = list()
+    for i in range(2, len(links), 6):
+        homeLinks.append(links[i].get("href"))
+        guestLinks.append(links[i + 1].get("href"))
+        spielLinks.append(links[i + 2].get("href"))
 
-    # In[64]:
+    # Links zur Gesamttabelle hinzufügen
+    if len(df.columns) == len(homeLinks):
+        df.insert(len(df.columns), "home_link", homeLinks)
+        df.insert(len(df.columns), "guest_link", guestLinks)
+        df.insert(len(df.columns), "Spiel", spielLinks)
 
+    numOfGames += df.shape[0]
 
-    df
-
-
-    # In[65]:
-
-
-    links = table.find_all('a')
-    homeLinks = list();
-    guestLinks = list();
-    spielLinks = list();
-    for i in range(2, len(links),6):
-        homeLinks.append(links[i].get('href'))
-        guestLinks.append(links[i+1].get('href'))
-        spielLinks.append(links[i+2].get('href'))
-    df.insert(len(df.columns), "home_link", homeLinks)
-    df.insert(len(df.columns), 'guest_link', guestLinks)
-    df.insert(len(df.columns), 'Spiel', spielLinks)
-    df
-
-
-    # In[66]:
-    current_date = datetime.date.today()
-    date_string = current_date.strftime('%Y-%m-%d')
-    #merge columns to table
-    if (idx==0):
+    # Gesamttabelle als csv abspeichern
+    # wenn 1. Link, dann wird alte csv-Datei überschrieben
+    # ansonsten werden die Tabellen einfach hinten dran gehängt, so dass sich am Ende alle Spiele
+    # von allen Mannschaften in einer Datei befinden
+    if idx == 0:
         df.to_csv(outfile, index=False)
     else:
-        df.to_csv(outfile, mode='a', index=False, header=False)
+        df.to_csv(outfile, mode="a", index=False, header=False)
 
+print("number of games: " + str(numOfGames))
 
+df = pd.read_csv(outfile, sep=",")
+df.head()
+weeks = sorted(df.KW.unique())
 
+for kw in weeks:
+    print("generate table " + str(kw))
+    filename = "Spiele_KW_" + str(kw) + ".html"
+    # extract data from current calendar week
+    kw_data = df.loc[df["KW"] == kw]
+    # sort data by date and time
+    kw_data = kw_data.sort_values(by=["Datum", "Zeit"])
 
+    # write head of table in html
+    table_head = '<table id="Spiele">\n'
+    table_head += "\t<tr>\n"
+    table_head += "\t\t<th>Datum</th>\n"
+    table_head += "\t\t<th>ABI Team</th>\n"
+    table_head += "\t\t<th>Heim</th>\n"
+    table_head += "\t\t<th>Gast</th>\n"
+    table_head += "\t\t<th>Ergebnis</th>\n"
+    table_head += "\t</tr>"
+    # print(table_head)
+    f = open(filename, "w")
+    f.write(table_head)
+    f.close()
+    kw_data.head()
 
+    f = open(filename, "a")
+    for ind in kw_data.index:
+        html_line = "\t<tr>\n"
+        html_line += "\t\t<td>" + df["Datum"][ind] + ", " + df["Zeit"][ind] + "</td>\n"
+        html_line += "\t\t<td>" + df["Team"][ind] + "</td>\n"
+        html_line += (
+            "\t\t<td>"
+            + '<a href="'
+            + df["home_link"][ind]
+            + '" target="_blank">'
+            + df["Heim"][ind].replace("\u200b", "")
+            + "</a></td>\n"
+        )
+        html_line += (
+            "\t\t<td>"
+            + '<a href="'
+            + df["guest_link"][ind]
+            + '" target="_blank">'
+            + df["Gast"][ind].replace("\u200b", "")
+            + "</a></td>\n"
+        )
+        html_line += (
+            "\t\t<td>"
+            + '<a href="'
+            + df["Spiel"][ind]
+            + '" target="_blank">link</a>'
+            + "</td>\n"
+        )
+        html_line += "\t</tr>\n"
+
+        f.write(html_line)
+    f.write("</table>")
+    f.close()
